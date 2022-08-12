d2afb5bb76ba2bd552374c30eedeb04ce8d4d05c
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2022-08-11 21:57:53 -0300

    feat: add url metadata info into postsRouter

--- src/routes/postsRouter.js
+++ src/routes/postsRouter.js
@@ -3,10 +3,11 @@ import { requireToken } from '../services/requireToken.js';
 import { publishPost , getPosts} from '../controllers/postsController.js';
 import { validateSchema } from "../middlewares/schemaValidator.js";
 import { postsSchema } from '../schemas/postsSchema.js';
+import { getUrlMetadata } from '../services/getUrlMetadata.js';
 
 const postsRouter = Router();
 
-postsRouter.post("/timeline", validateSchema(postsSchema), requireToken, publishPost);
+postsRouter.post("/timeline", validateSchema(postsSchema), requireToken, getUrlMetadata, publishPost);
 postsRouter.get("/timeline", getPosts);
 
 export default postsRouter;
