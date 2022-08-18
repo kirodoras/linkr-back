66c529a202c1e2755b846416e294ded1dcf645d9
luigi tanaka <luigi.tanaka12@gmail.com>
2022-08-17 21:59:21 -0300

    searchBar fazendo busca com usuarios seguidos no topo

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -5,7 +5,7 @@ import { getUsersByName, getPostsUser } from "../controllers/usersController.js"
 const usersRouter = Router();
 
 //colocar token
-usersRouter.get("/users", requireToken, getUsersByName);
+usersRouter.get("/users/:id", requireToken, getUsersByName);
 usersRouter.get("/user/:id", getPostsUser);
 
 export default usersRouter;
