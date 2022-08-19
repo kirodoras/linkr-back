d98181eda4dd9b583056f7969ea833e263adec84
luigi tanaka <luigi.tanaka12@gmail.com>
2022-08-19 17:24:58 -0300

    feature de comentarios finalizada

--- src/routes/commentRouter.js
+++ src/routes/commentRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { postComment , getAmountOfComments} from "../controllers/commentController.js";
+import { postComment , getAmountOfComments, getComments } from "../controllers/commentController.js";
 import { requireToken } from '../services/requireToken.js';
 import { validateSchema } from "../middlewares/schemaValidator.js";
 import { commentSchema } from "../schemas/commentSchema.js";
@@ -8,5 +8,6 @@ const commentRouter = Router();
 
 commentRouter.post("/comment/:postId", validateSchema(commentSchema), requireToken, postComment);
 commentRouter.get("/comment/:postId", getAmountOfComments);
+commentRouter.get("/comments/:postId", getComments);
 
 export default commentRouter;
