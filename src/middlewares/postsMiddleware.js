fca9f51f22d40d4f56f2809901ae179176e71988
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2022-08-19 18:26:45 -0300

    refactor: check post is not owner

--- src/middlewares/postsMiddleware.js
+++ src/middlewares/postsMiddleware.js
@@ -13,3 +13,17 @@ export async function checkPostOwner(req, res, next) {
         res.status(500).send(error.message);
     }
 }
+
+export async function checkPostIsNotOwner(req, res, next) {
+    try {
+        const { postId } = req.params;
+        const { userId } = res.locals;
+        const { rows: post } = await selectPostById(postId);
+        if (post[0].userId === userId) {
+            return res.sendStatus(403);
+        }
+        next();
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+}
