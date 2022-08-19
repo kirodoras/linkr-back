5b5a0a1a43faffe339afb4c0085e09c108c352e7
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2022-08-19 18:17:44 -0300

    chore: add check post owner in route post/share/:postId

--- src/routes/shareRouter.js
+++ src/routes/shareRouter.js
@@ -2,10 +2,10 @@ import { Router } from "express";
 import { sharePost , getAmountOfShares} from "../controllers/shareController.js";
 import { requireToken } from '../services/requireToken.js';
 import { checkIfAlreadyShare } from "../middlewares/shareMiddleware.js";
-
+import { checkPostOwner } from "../middlewares/postsMiddleware.js";
 const shareRouter = Router();
 
-shareRouter.post("/share/:postId", requireToken, checkIfAlreadyShare, sharePost);
+shareRouter.post("/share/:postId", requireToken, checkPostOwner, checkIfAlreadyShare, sharePost);
 shareRouter.get("/share/:postId", getAmountOfShares);
 
 export default shareRouter;
